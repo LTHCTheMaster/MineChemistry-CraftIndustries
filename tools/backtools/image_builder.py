9ec603ef741de049a78d17f6d28945c791ef8a41
CHOSSY Lucas <mistermega52@gmail.com>
2023-08-31 18:00:47 +0200

    loot tables

--- tools/backtools/image_builder.py
+++ tools/backtools/image_builder.py
@@ -43,9 +43,9 @@ class Run:
 				pred = []
 				elements.saveElements(FIXED_PATH[1])
 				for j, i in enumerate(elements.elements):
-					self.gdm.set_to_json(FIXED_PATH[0] + i.file_name + '.json', {"parent": "item/generated","textures": {"layer0": f"lthc.chemistry:block/elements/{i.file_name}"}}, True)
+					self.gdm.set_to_json(FIXED_PATH[0] + i.file_name + '.json', {"parent": "item/generated","textures": {"layer0": f"lthc.chemistry:block/elements/{i.file_name}"}}, False)
 					pred.append({"predicate": {"custom_model_data": 170000 + j},"model": "lthc.chemistry:elements/" + i.file_name})
-				self.gdm.set_to_json(FIXED_PATH[2], {"parent": "minecraft:block/repeating_command_block","overrides": [i for i in pred]}, True)
+				self.gdm.set_to_json(FIXED_PATH[2], {"parent": "minecraft:block/repeating_command_block","overrides": [i for i in pred]}, False)
 			case _:
 				pass
 	
--- tools/backtools/registry_writer.py
+++ tools/backtools/registry_writer.py
@@ -1,10 +1,11 @@
 from .image_builder import elements
+from datargsing import dGDM as GDM
 
-FIXED_PATH: tuple[str] = ("Datapack/data/lthc.chemistry/functions/init/_intern/sub_parts/parts/registry/elements.mcfunction",)
+FIXED_PATH: tuple[str] = ("Datapack/data/lthc.chemistry/functions/init/_intern/sub_parts/parts/registry/elements.mcfunction","Datapack/data/lthc.chemistry/loot_tables/i/elements/")
 
 class Run:
 	def __init__(self):
-		pass
+		self.gdm = GDM()
 
 	def run(self, cmd: list[str]):
 		match cmd[0]:
@@ -12,15 +13,17 @@ class Run:
 				file_content = "## ELEMENTS\n"
 				register_assembly: list[str] = []
 				for numlowered, element in enumerate(elements.elements):
+					calculated = 170000 + numlowered
 					tmp = "# " + element.name.capitalize() + '\n'
-					tmp += f"data modify storage lthc.chemistry:main REGISTRY.Items.{170000 + numlowered} "
+					tmp += f"data modify storage lthc.chemistry:main REGISTRY.Items.{calculated} "
 					tmp += 'set value {Slot:16b,id:"minecraft:repeating_command_block",Count:1b,tag:{ctc:{id:"'
-					tmp += element.name
+					tmp += element.name.lower()
 					tmp += '",from:"lthc.chemistry",traits:{element:1b}}'
-					tmp += f",CustomModelData:{170000 + numlowered},lthc_chemistry:"
+					tmp += f",CustomModelData:{calculated},lthc_chemistry:"
 					tmp += '{' + f"{element.state}:1b,{element.natural_occurence}:1b"
-					tmp += '},display:{Lore:[\'{"translate":"lthcthemaster.lthc.chemistry.lore.tooltip","color":"blue","italic":true}\'],Name:\'[{"translate":"lthcthemaster.lthc.chemistry.items.elements.' + element.name + '","italic":false,"color":"#ffffff""}]\'}}}'
+					tmp += '},display:{Lore:[\'{"translate":"lthcthemaster.lthc.chemistry.lore.tooltip","color":"blue","italic":true}\'],Name:\'[{"translate":"lthcthemaster.lthc.chemistry.items.elements.' + element.name.lower() + '","italic":false,"color":"#ffffff""}]\'}}}'
 					register_assembly.append(tmp)
+					self.gdm.set_to_json(FIXED_PATH[1] + element.name.lower() + '.json', {"pools": [{"rolls": 1,"bonus_rolls": 0,"entries": [{"type": "minecraft:item","name": "minecraft:repeating_command_block","functions": [{"function": "minecraft:copy_nbt","source": {"type": "minecraft:storage","source": "lthc.chemistry:main"},"ops": [{"source": "REGISTRY.Items."+ str(calculated) +".tag","target": "{}","op": "merge"}]}]}]}]}, True)
 				file_content += '\n'.join(register_assembly)
 				file = open(FIXED_PATH[0], "w", encoding='utf-8')
 				file.write(file_content)
