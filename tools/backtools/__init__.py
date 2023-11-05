975b35b6875a0c688c4f627ac1ea0efac733cf6d
CHOSSY Lucas <mistermega52@gmail.com>
2023-11-05 21:51:43 +0100

    translation added

--- tools/backtools/__init__.py
+++ tools/backtools/__init__.py
@@ -1,2 +1,4 @@
 from .image_builder import Run as IMGRun
 from .registry_writer import Run as REGRun
+from .translation_extractor import ghfedhbjhrruyghruuytgeutyetdyhtueuhgufytyru
+del ghfedhbjhrruyghruuytgeutyetdyhtueuhgufytyru
