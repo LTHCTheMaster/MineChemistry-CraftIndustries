ec572273699c11fef35c6f147e47c392669803a4
CHOSSY Lucas <mistermega52@gmail.com>
2024-01-03 16:11:38 +0100

    *

--- cleanworkspace.bat
+++ cleanworkspace.bat
@@ -4,6 +4,9 @@ del /Q elementsBuildTest
 del /Q elementsBuildTestdust
 del /Q elementsBuildTestimported
 del /Q elementsBuildTestingot
+del /Q elementsBuildTestdust_block
+del /Q elementsBuildTestimported_block
+del /Q elementsBuildTestingot_block
 del /Q periodicBuildTest
 del /Q templatesExport
 del /Q exportTest
@@ -12,8 +15,13 @@ del /Q Datapack\data\lthc.chemistry\loot_tables\i\elements\solid\*.json
 del /Q Resourcepack\assets\lthc.chemistry\models\elements\*.json
 del /Q Resourcepack\assets\lthc.chemistry\models\elements\dust\*.json
 del /Q Resourcepack\assets\lthc.chemistry\models\elements\ingot\*.json
+del /Q Resourcepack\assets\lthc.chemistry\models\elements\imported\*.json
 del /Q Resourcepack\assets\lthc.chemistry\textures\block\elements\*.png
 del /Q Resourcepack\assets\lthc.chemistry\textures\block\elements\dust\*.png
 del /Q Resourcepack\assets\lthc.chemistry\textures\block\elements\ingot\*.png
+del /Q Resourcepack\assets\lthc.chemistry\textures\block\elements\imported\*.png
+del /Q Resourcepack\assets\lthc.chemistry\textures\block\elements\dust_block\*.png
+del /Q Resourcepack\assets\lthc.chemistry\textures\block\elements\ingot_block\*.png
+del /Q Resourcepack\assets\lthc.chemistry\textures\block\elements\imported_block\*.png
 cls
 @echo on
