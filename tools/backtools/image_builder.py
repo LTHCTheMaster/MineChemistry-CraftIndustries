48a4754581603eeac557993d037722b2ca88231d
CHOSSY Lucas <mistermega52@gmail.com>
2024-01-04 08:59:57 +0100

    block models

--- tools/backtools/image_builder.py
+++ tools/backtools/image_builder.py
@@ -1,10 +1,12 @@
 """
 Image commands ?
 """
-from .core import PeriodicTableBuilder, elements, TemplateImageExporter
+from .core import PeriodicTableBuilder, elements, TemplateImageExporter, DustBlockTextureImage, IronBlockTextureImage, CopperBlockTextureImage, GoldenBlockTextureImage
 from datargsing import dGDM as GDM
+from PIL import Image
 
-FIXED_PATH: tuple[str] = ["Resourcepack/assets/lthc.chemistry/models/elements/", "Resourcepack/assets/lthc.chemistry/textures/block/elements/", "Resourcepack/assets/minecraft/models/item/repeating_command_block.json"]
+FIXED_PATH: tuple[str] = ("Resourcepack/assets/lthc.chemistry/models/elements/", "Resourcepack/assets/lthc.chemistry/textures/block/elements/", "Resourcepack/assets/minecraft/models/item/repeating_command_block.json")
+FIXED_PATH2: tuple[str] = ("Resourcepack/assets/lthc.chemistry/models/block/", "Resourcepack/assets/minecraft/models/item/cobblestone.json", "Resourcepack/assets/minecraft/models/item/barrel.json")
 
 class Run:
 	"""
@@ -63,7 +65,10 @@ class Run:
 			case "rp":
 				pred = []
 				pred2 = []
+				pred3 = []
+				pred4 = []
 				to_add = 0
+				to_add2 = 0
 				elements.saveElements(FIXED_PATH[1])
 				print("\033[35mTextures:\033[0m \033[32mDone\033[0m")
 				for j, i in enumerate(elements.elements):
@@ -73,12 +78,25 @@ class Run:
 						if i.shape in ("ingot", "dust"):
 							self.gdm.set_to_json(FIXED_PATH[0] + i.shape + '/' + i.file_name + '.json', {"parent": "item/generated","textures": {"layer0": f"lthc.chemistry:block/elements/{i.shape}/{i.file_name}"}}, False)
 							pred2.append({"predicate": {"custom_model_data": 170118 + to_add},"model": "lthc.chemistry:elements/" + i.shape + "/" + i.file_name})
+							if isinstance(i.block_image, (DustBlockTextureImage, IronBlockTextureImage, CopperBlockTextureImage, GoldenBlockTextureImage)):
+								pred3.append({"predicate": {"custom_model_data": 172000 + to_add2},"model": "lthc.chemistry:block/" + i.file_name})
+								pred4.append({"predicate": {"custom_model_data": 172000 + to_add2},"model": "lthc.chemistry:block/barrel/" + i.file_name})
+								self.gdm.set_to_json(FIXED_PATH2[0] + i.file_name + '.json', {"parent": "lthc.chemistry:block_renderer", "textures": {"all": f"lthc.chemistry:block/elements/{i.shape}_block/{i.file_name}"}})
+								self.gdm.set_to_json(FIXED_PATH2[0] + 'barrel/' + i.file_name + '.json', {"parent": "minecraft:block/cube_all", "textures": {"all": f"lthc.chemistry:block/elements/{i.shape}_block/{i.file_name}"}})
 						else:
 							self.gdm.set_to_json(FIXED_PATH[0] + 'imported/' + i.file_name + '.json', {"parent": "item/generated","textures": {"layer0": f"lthc.chemistry:block/elements/imported/{i.file_name}"}}, False)
 							pred2.append({"predicate": {"custom_model_data": 170118 + to_add},"model": "lthc.chemistry:elements/imported/" + i.file_name})
+							if isinstance(i.block_image, Image.Image):
+								pred3.append({"predicate": {"custom_model_data": 172000 + to_add2},"model": "lthc.chemistry:block/" + i.file_name})
+								pred4.append({"predicate": {"custom_model_data": 172000 + to_add2},"model": "lthc.chemistry:block/barrel/" + i.file_name})
+								self.gdm.set_to_json(FIXED_PATH2[0] + i.file_name + '.json', {"parent": "lthc.chemistry:block_renderer", "textures": {"all": f"lthc.chemistry:block/elements/imported_block/{i.file_name}"}})
+								self.gdm.set_to_json(FIXED_PATH2[0] + 'barrel/' + i.file_name + '.json', {"parent": "minecraft:block/cube_all", "textures": {"all": f"lthc.chemistry:block/elements/imported_block/{i.file_name}"}})
 						to_add += 1
+						to_add2 += 1
 				pred.extend(pred2)
 				self.gdm.set_to_json(FIXED_PATH[2], {"parent": "minecraft:block/repeating_command_block","overrides": [i for i in pred]}, False)
+				self.gdm.set_to_json(FIXED_PATH2[1], {"parent": "block/cobblestone","overrides": [i for i in pred3]}, False)
+				self.gdm.set_to_json(FIXED_PATH2[2], {"parent": "block/barrel","overrides": [i for i in pred4]}, False)
 				print("\033[35mModels:\033[0m \033[32mDone\033[0m")
 			case _:
 				pass
